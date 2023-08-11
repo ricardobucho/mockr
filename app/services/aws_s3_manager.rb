3406eefea914a3e743d50a358a3468f4ba9c2e4a
Ricardo Bucho <ricardobucho@live.com>
2023-08-11 21:28:43 +0100

    Remove extra configs

--- app/services/aws_s3_manager.rb
+++ app/services/aws_s3_manager.rb
@@ -24,19 +24,6 @@ class AwsS3Manager
       region: ENV.fetch("AWS_REGION", nil),
       access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID", nil),
       secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY", nil),
-      **role_credentials,
     )
-  end
-
-  def role_credentials
-    return {} if ENV.fetch("AWS_ROLE_ARN", nil).blank?
-
-    {
-      credentials: Aws::AssumeRoleCredentials.new(
-        client: Aws::STS::Client.new,
-        role_arn: ENV.fetch("AWS_ROLE_ARN", nil),
-        role_session_name: ENV.fetch("AWS_ROLE_SESSION_NAME", nil),
-      ),
-    }
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,13 @@
 
 Rails.application.config.middleware.use(OmniAuth::Builder) do
   provider(
+    :github,
+    ENV.fetch("GITHUB_CLIENT_ID", nil),
+    ENV.fetch("GITHUB_CLIENT_SECRET", nil),
+    scope: "user:email,user:profile,read:org",
+  )
+
+  provider(
     :okta,
     ENV.fetch("OKTA_CLIENT_ID", nil),
     ENV.fetch("OKTA_CLIENT_SECRET", nil),
